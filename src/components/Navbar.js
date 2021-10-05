3650849e27024b09dc112ec8a41a9e5145732674
adarsh sharma <sharma2015adarsh@gmail.com>
2021-10-05 09:12:50 +0530

    start project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-function Navbar() { <
-    >
-    <
-    nav className = "navbar" >
-        <
-        div className = "navbar-container" > < /div> <
-        Link to = "/"
-    className = "navbar-logo" >
-        trvl <
-        /Link> <
-        /nav> <
-        />;
+function Navbar() {
+  <>
+    <nav className="navbar">
+      <div className="navbar-container"> </div>
+      <Link to="/" className="navbar-logo">
+        trvl
+      </Link>
+    </nav>
+  </>;
 }
 
 export default Navbar;
