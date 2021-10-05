bf9a5667f1536ced5d580ae64ddc0ebb8f7603e7
adarsh sharma <sharma2015adarsh@gmail.com>
2021-10-05 07:23:17 +0530

    start of project

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() { <
+    >
+    <
+    nav className = "navbar" >
+        <
+        div className = "navbar-container" > < /div> <
+        Link to = "/"
+    className = "navbar-logo" >
+        trvl <
+        /Link> <
+        /nav> <
+        />;
+}
+
+export default Navbar;
